6d8c9e8fa7a833fa7a1e85af8c684489a157e701
Torito9609 <toroandres85@gmail.com>
2025-03-16 13:55:17 -0500

    algunos cambios anadidos a la vista

--- src/co/edu/unbosque/view/PanelLateral.java
+++ src/co/edu/unbosque/view/PanelLateral.java
@@ -1,8 +1,11 @@
 package co.edu.unbosque.view;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -24,22 +27,44 @@ class PanelLateral extends JPanel {
     public PanelLateral() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.DARK_GRAY);
+        setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        
+        Font tituloFont = new Font("Arial", Font.BOLD, 16);
+        Font tiempoFont = new Font("Arial", Font.BOLD, 24);
 
         tituloTiempo = new JLabel("Tiempo en partida");
         tituloTiempo.setForeground(Color.WHITE);
+        tituloTiempo.setFont(tituloFont);
+        
         tiempo = new JLabel("00:00:00");
         tiempo.setForeground(Color.WHITE);
+        tiempo.setFont(tiempoFont);
+        tiempo.setAlignmentX(CENTER_ALIGNMENT);
         
         tituloHistorial = new JLabel("Historial");
         tituloHistorial.setForeground(Color.WHITE);
+        tituloHistorial.setFont(tituloFont);
+        tituloHistorial.setAlignmentX(CENTER_ALIGNMENT);
         
         historial = new JTextArea(10, 20);
         historial.setEditable(false);
+        JScrollPane scrollHistorial = new JScrollPane(historial);
+        scrollHistorial.setAlignmentX(CENTER_ALIGNMENT);
+        scrollHistorial.setMaximumSize(new Dimension(250, 150));
         
         pausarBtn = new JButton("Pausar");
         reiniciarBtn = new JButton("Reiniciar");
         borrarHistorialBtn = new JButton("Borrar historial");
         salirBtn = new JButton("Salir");
+
+        JButton[] botones = {pausarBtn, reiniciarBtn, borrarHistorialBtn, salirBtn};
+        
+        for (JButton btn : botones) {
+            btn.setBackground(Color.RED);
+            btn.setForeground(Color.WHITE);
+            btn.setAlignmentX(CENTER_ALIGNMENT);
+            btn.setMaximumSize(new Dimension(200, 40));
+        }
         
         add(tituloTiempo);
         add(tiempo);
